6b67c04ac1f63eeae299f82ce031238ca4d677dc
akshayaa-2024-aia <akshayaasudhaharan13@gmail.com>
2024-12-04 21:49:42 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,20 @@
+Dividend: 3
+Divisor: 3
+Result: 1.0
+Execution Time: Tue Dec  3 22:26:36 2024
+No error occurred
+Code is executed successfully
+
+Dividend: 12
+Divisor: 4
+Result: 3.0
+Execution Time: Tue Dec  3 22:28:16 2024
+No error occurred
+Code is executed successfully
+
+Error: division by zero
+Code is executed successfully
+
+Error: division by zero
+Code is executed successfully
+
